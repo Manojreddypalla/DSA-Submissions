b3d2debe2af32d1e6dd1f887135127adb4a400e0
Manoj Reddy <151192226+Manojreddypalla@users.noreply.github.com>
2026-05-16 21:48:17 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n);
+
+        dp[0]=nums[0];
+
+
+
+        for(int i = 1 ; i < n ; i++)
+        {
+            int take=nums[i];
+        
+        if(i>1)
+        {
+            take+=dp[i-2];
+        }
+
+        int notTaken=dp[i-1];
+        dp[i]=max(take,notTaken);
+
+        }
+        return dp[n-1];
+    }
+};
