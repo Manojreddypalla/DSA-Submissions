e437670c507a536b161cb31895e771bb9bbe3fb9
Manoj Reddy <151192226+Manojreddypalla@users.noreply.github.com>
2026-05-23 14:31:26 +0530

    Time: 0 ms (100%), Space: 21.9 MB (37.48%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        int n=nums.size();
+       int exp = n*(n+1)/2;
+        return exp-sum;
+       
+    }
+};
