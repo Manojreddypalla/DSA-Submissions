1d4133d01a0d2902f10eadd692e8b4724e6567a4
Manoj Reddy <151192226+Manojreddypalla@users.noreply.github.com>
2026-06-01 20:58:46 +0530

    Add: top-k-elements-in-list - submission-12

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-12.cpp
@@ -0,0 +1,68 @@
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//        unordered_map<int,int> mp;
+//        vector<pair<int,int>> p;
+//        vector<int> res;
+//        for(auto m:nums){
+//         mp[m]++;
+//        }
+//        for(auto n:mp)
+//        {
+//         p.push_back({n.second,n.first});
+//        }
+//        sort(p.rbegin(),p.rend());
+//        for(int i=0;i<k;i++)
+//        {
+//         res.push_back(p[i].second);
+
+//        }
+//        return res;
+
+//     }
+// };
+class Solution {
+public:
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        // {number, frequency}
+        vector<pair<int,int>> p;
+
+        for(int num : nums) {
+
+            bool found = false;
+
+            // search if number already exists
+            for(auto &pair : p) {
+
+                if(pair.first == num) {
+
+                    pair.second++;
+                    found = true;
+                    break;
+                }
+            }
+
+            // if number not found
+            if(!found) {
+                p.push_back({num, 1});
+            }
+        }
+
+        // sort by frequency descending
+        sort(p.begin(), p.end(),
+            [](pair<int,int>& a, pair<int,int>& b) {
+                return a.second > b.second;
+            });
+
+        vector<int> res;
+
+        // take top k numbers
+        for(int i = 0; i < k; i++) {
+            res.push_back(p[i].first);
+        }
+
+        return res;
+    }
+};
