09c882200f3359871ada90766c6e34fe6383a857
retr0 <151192226+Manojreddypalla@users.noreply.github.com>
2026-09-05 12:58:44 +0530

    Add: house-robber-ii - submission-13

---
+++ Data Structures & Algorithms/house-robber-ii/submission-13.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int solve(int n,vector<int>&nums,vector<int>&dp)
+    {
+        if(n==0)
+        {
+            return nums[0];
+        }
+        if(n<0)
+        {
+            return 0;
+        }
+
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        int run1=nums[n]+solve(n-2,nums,dp);
+        int run2=solve(n-1,nums,dp);
+        return dp[n]=max(run1,run2);
+    }
+    int rob(vector<int>& nums) {
+       int n=nums.size();
+        if(n==1)
+        {
+            return nums[0];
+        }
+        
+        vector<int>run1(nums.begin(),nums.end()-1);
+        vector<int>run2(nums.begin()+1,nums.end());
+        //we are gonna do 2 dp tooo hhere 
+        vector<int>dp1(run1.size(),-1);
+        vector<int>dp2(run2.size(),-1);
+
+        int rob1=solve(run1.size()-1,run1,dp1);
+        int rob2=solve(run2.size()-1,run2,dp2);
+        return max(rob1,rob2);
+        
+    }
+};
